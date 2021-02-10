660cc3170df3a1db6e1926f970fde2cccf620b53
BryanGK <bryangordonkrauss@gmail.com>
2021-02-09 16:43:51 -0800

    Trying to work out some bugs with the = operator

--- script.js
+++ script.js
@@ -10,70 +10,71 @@ const btnSeven = document.querySelector("#btn-7");
 const btnEight = document.querySelector("#btn-8");
 const btnNine = document.querySelector("#btn-9");
 const btnZero = document.querySelector("#btn-0");
-const btnClear = document.querySelector("#btn-clear");
 const btnDot = document.querySelector("#btn-dot");
 const btnEqual = document.querySelector("#btn-equal");
 const btnAdd = document.querySelector("#btn-add");
 const btnSub = document.querySelector("#btn-sub");
 const btnDivide = document.querySelector("#btn-divide");
 const btnMulti = document.querySelector("#btn-multi");
-const display = document.querySelector("output");
+const display = document.querySelector("#display");
+const btnClear = document.querySelector("#btn-clear");
+
 
 const values = {
-    num1: 0,
-    num2: 0,
+    num1: "",
+    num2: "",
     operator: "",
-    result: 0,
+    result: "",
 };
 
 //All the individual number button eventlisteners
 
 btnOne.addEventListener('click', () => {
-    display.innerHTML += 1;
+    display.innerText += 1;
 });
 btnTwo.addEventListener('click', () => {
-    display.innerHTML += 2;
+    display.innerText += 2;
 });
 btnThree.addEventListener('click', () => {
-    display.innerHTML += 3;
+    display.innerText += 3;
 });
 btnFour.addEventListener('click', () => {
-    display.innerHTML += 4;
+    display.innerText += 4;
 });
 btnFive.addEventListener('click', () => {
-    display.innerHTML += 5;
+    display.innerText += 5;
 });
 btnSix.addEventListener('click', () => {
-    display.innerHTML += 6;
+    display.innerText += 6;
 });
 btnSeven.addEventListener('click', () => {
-    display.innerHTML += 7;
+    display.innerText += 7;
 });
 btnEight.addEventListener('click', () => {
-    display.innerHTML += 8;
+    display.innerText += 8;
 });
 btnNine.addEventListener('click', () => {
-    display.innerHTML += 9;
+    display.innerText += 9;
 });
 btnZero.addEventListener('click', () => {
-    display.innerHTML += 0;
+    display.innerText += 0;
 });
 
 //Misc buttons
 
 btnClear.addEventListener('click', () => {
-    display.innerHTML = '';
-    values.num1 = 0;
-    values.num2 = 0;
+    display.innerText = '';
+    values.num1 = "";
+    values.num2 = "";
     values.operator = '';
-    values.result = 0;
+    values.result = "";
     console.log("Clear");
     console.log(values);
 });
 
 btnDot.addEventListener('click', () => {
-    if (!display.innerHTML.includes(".")) {
-        display.innerHTML += ".";   
+    if (!display.innerText.includes(".")) {
+        display.innerText += ".";   
     } else {
         return;
     }
@@ -81,100 +82,113 @@ btnDot.addEventListener('click', () => {
 
 // "Operate" button
 
-btnEqual.addEventListener('click', () => {
+btnEqual.addEventListener('click', operate);
+
+function operate() {
     if (values.operator === "+") {
-        values.num2 = parseFloat(display.innerHTML);
+        values.num2 = parseFloat(display.innerText);
         values.result = values.num1 + values.num2;
-        display.innerHTML = values.result;
+        values.num1 = values.result;
+        display.innerText = values.result;
         console.log("=");
         console.log(values);
-    }
-    if (values.operator === "-") {
-        values.num2 = parseFloat(display.innerHTML);
+    } else if (values.operator === "-") {
+        values.num2 = parseFloat(display.innerText);
         values.result = values.num1 - values.num2;
-        display.innerHTML = values.result;
+        values.num1 = values.result;
+        display.innerText = values.result;
         console.log("=");
         console.log(values);
-    }
-    if (values.operator === "/") {
+    } else if (values.operator === "/") {
         if (values.num2 === 0) {
-            display.innerHTML = "ERROR - Can't divide by 0"
+            display.innerText = "ERROR - Can't divide by 0"
         } else {
-            values.num2 = parseFloat(display.innerHTML);
+            values.num2 = parseFloat(display.innerText);
             values.result = values.num1 / values.num2;
-            display.innerHTML = values.result;
+            values.num1 = values.result;
+            display.innerText = values.result;
             console.log("=");
             console.log(values);
         }
-    }
-    if (values.operator === "*") {
-        values.num2 = parseFloat(display.innerHTML);
+    } else if (values.operator === "*") {
+        values.num2 = parseFloat(display.innerText);
         values.result = values.num1 * values.num2;
-        display.innerHTML = values.result;
+        values.num1 = values.result;
+        display.innerText = values.result;
         console.log("=");
         console.log(values);
     }
-});
+};
 
 //Operators
 
-btnAdd.addEventListener('click', () => {
-    if (values.num1 === 0) {
-        values.num1 = parseFloat(display.innerHTML);
+btnAdd.addEventListener('click', add);
+
+function add() {
+    if (values.operator === "") {
+        values.num1 = parseFloat(display.innerText);
         values.operator = "+";
         console.log("+");
     } else {
-        values.num2 = parseFloat(display.innerHTML);
+        values.num2 = parseFloat(display.innerText);
+        operate();
         values.operator = "+";
         console.log("+"); 
     }
-    display.innerHTML = "";
-    console.log("+");
-    console.log(values);
-});
+    display.innerText = "";
+    // console.log(values);
+};
 
-btnSub.addEventListener('click', () => {
-    if (values.num1 === 0) {
-        values.num1 = parseFloat(display.innerHTML);
+btnSub.addEventListener('click', sub);
+
+function sub() {
+    if (values.num1 === "") {
+        values.num1 = parseFloat(display.innerText);
         values.operator = "-";
         console.log("-");
     } else {
-        values.num2 = parseFloat(display.innerHTML);
+        values.num2 = parseFloat(display.innerText);
+        operate();
         values.operator = "-";
         console.log("-");
     }
-    display.innerHTML = "";
+    display.innerText = "";
     console.log(values);
-    console.log("-");
-});
+};
 
-btnDivide.addEventListener('click', () => {
-    if (values.num1 === 0) {
-        values.num1 = parseFloat(display.innerHTML);
+btnDivide.addEventListener('click', divide);
+
+function divide() {
+    if (values.num1 === "") {
+        values.num1 = parseFloat(display.innerText);
         values.operator = "/";
         console.log("/");
     } else {
-        values.num2 = parseFloat(display.innerHTML);
+        values.num2 = parseFloat(display.innerText);
+        operate();
         values.operator = "/";
         console.log("/");
     }
-    display.innerHTML = "";
-    console.log("/");
+    display.innerText = "";
     console.log(values);
-});
+};
 
-btnMulti.addEventListener('click', () => {
-    if (values.num1 === 0) {
-        values.num1 = parseFloat(display.innerHTML);
+btnMulti.addEventListener('click', multi);
+
+function multi() {
+    if (values.num1 === "") {
+        values.num1 = parseFloat(display.innerText);
         values.operator = "*";
         console.log("*");
     } else {
-        values.num2 = parseFloat(display.innerHTML);
+        values.num2 = parseFloat(display.innerText);
+        operate();
         values.operator = "*";
         console.log("*");
     }
-    display.innerHTML = "";
-    console.log("*");
+    display.innerText = "";
     console.log(values);
-});
+};
+
+
 
