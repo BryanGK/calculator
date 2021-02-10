b7167c5dccf6e5b37f6e79f3755bf6eed4ed7113
BryanGK <bryangordonkrauss@gmail.com>
2021-02-09 18:06:12 -0800

    Got rid of the individual evenlisteners with a for of loop. Changes around some styles

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 //Make the buttons active
 
-const btnOne = document.querySelector("#btn-1");
-const btnTwo = document.querySelector("#btn-2");
-const btnThree = document.querySelector("#btn-3");
-const btnFour = document.querySelector("#btn-4");
-const btnFive = document.querySelector("#btn-5");
-const btnSix = document.querySelector("#btn-6");
-const btnSeven = document.querySelector("#btn-7");
-const btnEight = document.querySelector("#btn-8");
-const btnNine = document.querySelector("#btn-9");
-const btnZero = document.querySelector("#btn-0");
 const btnDot = document.querySelector("#btn-dot");
 const btnEqual = document.querySelector("#btn-equal");
 const btnAdd = document.querySelector("#btn-add");
@@ -17,7 +7,9 @@ const btnSub = document.querySelector("#btn-sub");
 const btnDivide = document.querySelector("#btn-divide");
 const btnMulti = document.querySelector("#btn-multi");
 const display = document.querySelector("#display");
+const output = document.querySelector("#output");
 const btnClear = document.querySelector("#btn-clear");
+const btns = document.querySelectorAll('.btn-num');
 
 
 const values = {
@@ -25,40 +17,18 @@ const values = {
     num2: "",
     operator: "",
     result: "",
+    test: "",
 };
 
-//All the individual number button eventlisteners
 
-btnOne.addEventListener('click', () => {
-    display.innerText += 1;
-});
-btnTwo.addEventListener('click', () => {
-    display.innerText += 2;
-});
-btnThree.addEventListener('click', () => {
-    display.innerText += 3;
-});
-btnFour.addEventListener('click', () => {
-    display.innerText += 4;
-});
-btnFive.addEventListener('click', () => {
-    display.innerText += 5;
-});
-btnSix.addEventListener('click', () => {
-    display.innerText += 6;
-});
-btnSeven.addEventListener('click', () => {
-    display.innerText += 7;
-});
-btnEight.addEventListener('click', () => {
-    display.innerText += 8;
-});
-btnNine.addEventListener('click', () => {
-    display.innerText += 9;
-});
-btnZero.addEventListener('click', () => {
-    display.innerText += 0;
-});
+//Make buttons work
+
+for (let btn of btns) {
+    btn.addEventListener('click', () => {
+        display.innerText += btn.innerText;
+    })
+}
+
 
 //Misc buttons
 
@@ -136,7 +106,7 @@ function add() {
         console.log("+"); 
     }
     display.innerText = "";
-    // console.log(values);
+    console.log(values);
 };
 
 btnSub.addEventListener('click', sub);
